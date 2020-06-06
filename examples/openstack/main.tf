6051a0cc605fb37ad7e8016e0088dbfc1596f624
arshul <arshulmohd123@gmail.com>
2020-06-07 00:31:30 +0530

    Specify multiple IP address to allow SSH. Readme and example file updated. FIX #2

--- examples/openstack/main.tf
+++ examples/openstack/main.tf
@@ -1,5 +1,5 @@
 module "openstack" {
-  source = "git::https://github.com/c3g/genpipes_cloud.git//openstack"
+  source = "git::https://github.com/c3g/genpipes_cloud.git/openstack"
 
 
   # Slurm definition
@@ -8,12 +8,12 @@ module "openstack" {
   nb_users            = 10
   domain_name         = "brune"
   shared_storage_size = 30
-  home_fs_size        = 5
+  home_fs_size        = 10
   public_key_path     = "./cloud.pub"
 
 
-  # ssh firewall allowed, comma separated
-  fw_ssh_filter = "0.0.0.0/0" # get with curl ifconfig.co
+  # ssh firewall allowed - list (e.g. ["103.212.144.89/32","106.223.123.174/32"])
+  fw_ssh_filter = [] # get with curl ifconfig.co
 
 
   # OpenStack specifics
@@ -25,17 +25,17 @@ module "openstack" {
 }
 
 output "public_ip" {
-	value = "${module.openstack.ip}"
+	value = module.openstack.ip
 }
 
 output "domain_name" {
-	value = "${module.openstack.domain_name}"
+	value = module.openstack.domain_name
 }
 
 output "admin_passwd" {
-	value = "${module.openstack.admin_passwd}"
+	value = module.openstack.admin_passwd
 }
 
 output "guest_passwd" {
-	value = "${module.openstack.guest_passwd}"
+	value = module.openstack.guest_passwd
 }
